454a273c3623dd47557b08eb0217e93cce8166e4
rb <rafael.bonanno@gmail.com>
2023-03-07 12:39:27 +0100

    seed 2 movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Destroying movies..."
+Movie.destroy_all
+puts "Creating movies..."
+Movie.create!(
+  title: "Potato: the comeback",
+  release: Date.new(2002, 02, 02)
+)
+Movie.create!(
+  title: "Potato 4: mashed potatoes",
+  release: Date.new(2004, 04, 04)
+)
+puts "Finished!"
